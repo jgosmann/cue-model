2219ba6c21079aef61ce31e0c2e3625d4e7cabf0
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-10-18 16:24:25 -0400

    Cleanup legacy context code.

--- imem/model/legacy/context.py
+++ imem/model/legacy/context.py
@@ -98,98 +98,6 @@ def AlternatingMemoryBuffers(d, beta, **kwargs):
     return net
 
 
-def ThreeMemory(d, beta, **kwargs):
-    kwargs.setdefault('label', "context.ThreeMemory")
-
-    with nengo.Network(**kwargs) as net:
-        net.input = nengo.Node(size_in=d)
-
-        net.new_ctx = nengo.Node(size_in=d)
-        net.current = GatedMemory(d)
-        net.buf = GatedMemory(d)
-        net.old = GatedMemory(d)
-
-        nengo.Connection(net.input, net.new_ctx, transform=beta)
-        nengo.Connection(
-            net.old.mem.output, net.new_ctx, transform=np.sqrt(1. - (beta)**2))
-        nengo.Connection(net.new_ctx, net.current.diff.input)
-        nengo.Connection(net.current.mem.output, net.buf.diff.input)
-        nengo.Connection(net.buf.mem.output, net.old.diff.input)
-
-        net.bias = nengo.Node(1)
-        net.dot = spa.Compare(d)
-        with nengo.presets.ThresholdingEnsembles(0.0):
-            net.update_done = nengo.Ensemble(150, 1)
-            nengo.Connection(net.bias, net.update_done, transform=-beta + 0.1)
-        net.update_done_th = nengo.Node(size_in=1)
-        nengo.Connection(net.input, net.dot.input_a)
-        nengo.Connection(net.current.mem.output, net.dot.input_b)
-        nengo.Connection(net.dot.output, net.update_done)
-        nengo.Connection(
-            net.update_done, net.update_done_th, synapse=None,
-            function=lambda x: 1 if x >= 0.0 else 0)
-
-        nengo.Connection(net.update_done_th, net.current.input_store)
-        nengo.Connection(net.update_done_th, net.old.input_store)
-        nengo.Connection(net.bias, net.buf.input_store)
-        nengo.Connection(net.update_done_th, net.buf.input_store, transform=-1)
-
-        net.output = net.current.mem.output
-
-    return net
-
-
-def Context4(d, beta, **kwargs):
-    with nengo.Network(**kwargs) as net:
-        net.input = nengo.Node(size_in=d)
-
-        net.new_ctx = nengo.Node(size_in=d)
-        net.current = GatedMemory(d)
-        net.buf = GatedMemory(d)
-        net.old = GatedMemory(d)
-
-        nengo.Connection(net.input, net.new_ctx, transform=beta)
-        nengo.Connection(
-            net.old.mem.output, net.new_ctx,
-            transform=np.sqrt(1. - (beta)**2))
-        nengo.Connection(net.new_ctx, net.current.diff.input)
-        nengo.Connection(net.current.mem.output, net.buf.diff.input)
-        nengo.Connection(net.buf.mem.output, net.old.diff.input)
-
-        net.bias = nengo.Node(1)
-        net.input_update_context = nengo.Node(size_in=1)
-
-        nengo.Connection(net.bias, net.current.input_store)
-        nengo.Connection(net.bias, net.old.input_store)
-        nengo.Connection(
-            net.input_update_context, net.current.input_store, transform=-1.,
-            synapse=None)
-        nengo.Connection(
-            net.input_update_context, net.old.input_store, transform=-1.,
-            synapse=None)
-
-        nengo.Connection(net.input_update_context, net.buf.input_store)
-
-        # net.downscale = spa.State(d)
-        # nengo.Connection(net.buf.mem.output, net.downscale.input)
-        # nengo.Connection(
-            # net.downscale.output, net.buf.mem.input, transform=-.1)
-        # nengo.Connection(
-            # net.downscale.output, net.buf.diff.input, transform=-.1)
-        # sq = net.buf.mem.state_ensembles.add_output('sq', np.square)
-        # with nengo.presets.ThresholdingEnsembles(0.):
-            # net.downscale_activate = nengo.Ensemble(50, 1)
-        # nengo.Connection(net.bias, net.downscale_activate)
-        # nengo.Connection(
-            # sq, net.downscale_activate, transform=-np.ones((1, d)))
-        # inhibit_net(net.downscale_activate, net.downscale.state_ensembles,
-                    # strength=3)
-
-        net.output = net.current.mem.output
-
-    return net
-
-
 def Context5(d, beta, **kwargs):
     with nengo.Network(**kwargs) as net:
         net.input = nengo.Node(size_in=d)
@@ -213,21 +121,6 @@ def Context5(d, beta, **kwargs):
             synapse=None)
 
         nengo.Connection(net.input_update_context, net.old.input_store)
-
-        # net.downscale = spa.State(d)
-        # nengo.Connection(net.buf.mem.output, net.downscale.input)
-        # nengo.Connection(
-            # net.downscale.output, net.buf.mem.input, transform=-.1)
-        # nengo.Connection(
-            # net.downscale.output, net.buf.diff.input, transform=-.1)
-        # sq = net.buf.mem.state_ensembles.add_output('sq', np.square)
-        # with nengo.presets.ThresholdingEnsembles(0.):
-            # net.downscale_activate = nengo.Ensemble(50, 1)
-        # nengo.Connection(net.bias, net.downscale_activate)
-        # nengo.Connection(
-            # sq, net.downscale_activate, transform=-np.ones((1, d)))
-        # inhibit_net(net.downscale_activate, net.downscale.state_ensembles,
-                    # strength=3)
 
         net.output = net.current.mem.output
 
