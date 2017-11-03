63b78b701d8fddc1ea39ab7c3955684b9cf468bb
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-11-03 11:49:33 -0400

    Update gexf script.

--- scripts/gexf.py
+++ scripts/gexf.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     parser.add_argument('filename', type=str, nargs=1, help="output filename")
     args = parser.parse_args()
 
-    proto = protocols.Recall(
-        pi=1., ipi=0., ri=0., serial=True, n_items=15, distractor_rate=1.)
-    vocabs = Vocabularies(proto, 64, 64, proto.n_items)
-    model = IMem(proto, vocabs, 0.62676, 0.9775, 0.)
+    proto = protocols.PROTOCOLS['serial']
+    stim_provider = protocols.StimulusProvider(proto, distractor_rate=1.)
+    vocabs = Vocabularies(stim_provider, 64, 64, proto.n_items)
+    model = IMem(stim_provider, vocabs, 0.62676, 0.9775, 0.)
 
     CollapsingGexfConverter().convert(model).write(args.filename[0])
